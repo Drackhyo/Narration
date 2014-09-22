f0575c05c49ca0d3daff22f3a9ccef561e7388f4
Drackhyo <drackhyo@gmail.com>
2014-09-22 00:45:43 -0400

    script create

--- Assets/Scripts/Playerinf.cs
+++ Assets/Scripts/Playerinf.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Playerinv : MonoBehaviour {
+public class Playerinf : MonoBehaviour {
 
+	Dictionary<uint><string> availablePhrases;
+	List<uint> itemsList;
 	// Use this for initialization
 	void Start () {
 	
